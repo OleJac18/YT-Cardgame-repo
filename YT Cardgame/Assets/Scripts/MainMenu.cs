0c2ada635a5accb70da8b8462c4d9ef424a935e4
OleJac18 <160741775+OleJac18@users.noreply.github.com>
2024-09-18 19:56:23 +0200

    Startseite erstellen für Multiplayer
    Youtube Video 3

---
+++ YT Cardgame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private string gameplaySceneName = "Gameplay";
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+    }
+
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
