f07fbb21577275889c826d593c82a91bc433fdf8
OleJac18 <160741775+OleJac18@users.noreply.github.com>
2024-11-03 10:47:58 +0100

    Prefabs und CardDeck klickbar gemacht
    - Card Prefab erstellt
    - CardController angepasst
    - CardManager erweitert
    - CardDeckUI Script erstellt, damit man das Deck klicken kann

--- YT Cardgame/Assets/Spiel/Scripts/Manager/CardManager.cs
+++ YT Cardgame/Assets/Spiel/Scripts/Manager/CardManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private GameObject _spawnCardDeckPos;
+
+
     [SerializeField] private CardStack _cardStack;
 
     // Start is called before the first frame update
@@ -12,6 +16,37 @@ public class CardManager : MonoBehaviour
         _cardStack = new CardStack();
         _cardStack.CreateDeck();
         _cardStack.ShuffleCards();
+
+        CardDeckUI.OnCardDeckClicked += HandleCardDeckClicked;
+    }
+
+    private void HandleCardDeckClicked()
+    {
+        int drawnCard = _cardStack.DrawTopCard();
+
+        if (drawnCard != 100)
+        {
+            SpawnCard(drawnCard);
+        }
+        else
+        {
+            Debug.Log("Kartenstapel ist leer.");
+        }
+    }
+
+    private void SpawnCard(int topCardNumber)
+    {
+        GameObject spawnCard = Instantiate(_cardPrefab, _spawnCardDeckPos.transform.position, _spawnCardDeckPos.transform.rotation);
+
+        spawnCard.transform.SetParent(_spawnCardDeckPos.transform.parent);
+        spawnCard.transform.localScale = Vector3.one;
+
+        CardController controller = spawnCard.GetComponent<CardController>();
+        controller.SetCorrespondingDeck(Card.Stack.CARDDECK);
+        controller.SetCardBackImageVisibility(true);
+        controller.CardNumber = topCardNumber;
+
+        LeanTween.move(spawnCard, new Vector3(300, 300, 0), 0.5f);
     }
 
 }
