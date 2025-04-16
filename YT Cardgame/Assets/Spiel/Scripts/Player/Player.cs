57a86e6f0b9a415d80b12a4264a49ab6921800f2
cajonsun <45065163+cajonsun@users.noreply.github.com>
2025-04-16 18:44:06 +0200

    Serialisierung der Player Klasse
    - Einbinden des INetworkSerializable Interfaces
    - Die Klasse serialisierbar und deserialisierbar machen

--- YT Cardgame/Assets/Spiel/Scripts/Player/Player.cs
+++ YT Cardgame/Assets/Spiel/Scripts/Player/Player.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using Unity.Netcode;
 
 [System.Serializable]
-public class Player
+public class Player : INetworkSerializable
 {
     public ulong id;
     public List<int> cards;
     public string name;
     public int score;
+
+    public Player() { }
 
     public Player(ulong id, List<int> cards, string name, int score)
     {
@@ -14,5 +17,34 @@ public class Player
         this.cards = cards;
         this.name = name;
         this.score = score;
+    }
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref id);
+        serializer.SerializeValue(ref name);
+        serializer.SerializeValue(ref score);
+
+        int count = serializer.IsWriter ? cards.Count : 0;
+        serializer.SerializeValue(ref count);
+
+        if (serializer.IsReader)
+        {
+            cards = new List<int>(count);
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            int card = serializer.IsWriter && i < cards.Count ? cards[i] : 0;
+            serializer.SerializeValue(ref card);
+
+            if (serializer.IsReader)
+            {
+                if (i < cards.Count)
+                cards[i] = card;
+                else
+                    cards.Add(card);
+            }
+        }
     }
 }
