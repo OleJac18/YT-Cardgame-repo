182578966cef79fa7925693fd0eb9bd8689996e4
cajonsun <45065163+cajonsun@users.noreply.github.com>
2025-04-19 11:36:48 +0200

    Player Klasse kommentiert

--- YT Cardgame_clone_0/Assets/Spiel/Scripts/Player/Player.cs
+++ YT Cardgame_clone_0/Assets/Spiel/Scripts/Player/Player.cs
@@ -25,9 +25,16 @@ public class Player : INetworkSerializable
         serializer.SerializeValue(ref name);
         serializer.SerializeValue(ref score);
 
+
+        // Senden: Länge der Liste abspeichern, um diese danach in den Stream zu schreiben
+        // Empfangen:  Es wird ein Defaultwert (0) in count geschrieben, weil 
+        //         list.Count noch null sein könnte, da sie entweder keine Elemente
+        //         hat oder noch nicht initalisiert worden ist
         int count = serializer.IsWriter ? cards.Count : 0;
         serializer.SerializeValue(ref count);
 
+        // Für das Lesen muss die Liste initalisiert sein, bevor auf sie in der
+        // for-Schleife zugegriffen wird. Könnte sonst null sein
         if (serializer.IsReader)
         {
             cards = new List<int>(count);
@@ -35,13 +42,18 @@ public class Player : INetworkSerializable
 
         for (int i = 0; i < count; i++)
         {
+            // Senden: Wert des Elements abspeichern, sofern der Index nicht 
+            //         außerhalb des Bereichs der Liste ist
+            // Empfangen:  Es wird ein Defaultwert (0) in card geschrieben, weil 
+            //         noch keine Elemente in der Liste abgespeichert sind.
             int card = serializer.IsWriter && i < cards.Count ? cards[i] : 0;
             serializer.SerializeValue(ref card);
 
+            // Beim Empfangen werden die einzelnen Elemente manuell in der Liste abgespeichert
             if (serializer.IsReader)
             {
                 if (i < cards.Count)
-                cards[i] = card;
+                    cards[i] = card;
                 else
                     cards.Add(card);
             }
